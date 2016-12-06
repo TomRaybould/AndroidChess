ac539b3e775a1c17fd8d63bd04350f58d3028b74
Mike Tomkowich <mike.tomkowich@nwk-77-76.nwk.ruw.rutgers.edu>
2016-12-06 14:34:56 -0500

    Switch back and forth between home and game

--- app/src/main/java/com/example/miketomkowich/chess72android/Home_Screen.java
+++ app/src/main/java/com/example/miketomkowich/chess72android/Home_Screen.java
@@ -3,11 +3,13 @@ package com.example.miketomkowich.chess72android;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Home_Screen extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +23,12 @@ public class Home_Screen extends AppCompatActivity {
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         layout.addView(textView);
     }
-
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Toast.makeText(getApplicationContext(), "switch screen was called", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, gamestage.class);
+        String message = "Back to gamestage";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }
